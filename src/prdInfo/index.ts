8cc6f84369c6760163f11ddaa8d70fa579559217
uxdx <uxdx@naver.com>
2023-08-31 22:45:16 +0900

    Directory name changed : prdinfo to prdInfo

---
+++ src/prdInfo/index.ts
@@ -0,0 +1,15 @@
+import PrdInfo from './PrdInfo';
+import PrdOption from './PrdOption';
+import PrdAttr from './PrdAttr';
+import PrdInfoBuilder from './PrdInfoBuilder';
+import PrdOptionBuilder from './PrdOptionBuilder';
+import PrdAttrBuilder from './PrdAttrBuilder';
+
+export {
+  PrdInfo,
+  PrdInfoBuilder,
+  PrdOptionBuilder,
+  PrdAttrBuilder,
+  PrdOption,
+  PrdAttr,
+};
