4b2198ccec0eb97993c825d62bfb90565af10fc4
uxdx <uxdx@naver.com>
2023-09-13 20:57:55 +0900

    1.0.8: UserData added.

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,3 +1,21 @@
+export type UserData = {
+  [key: string]: string | boolean | undefined;
+  uid?: string;
+  firstName?: string;
+  lastName?: string;
+  firstNameKana?: string;
+  lastNameKana?: string;
+  birth?: string;
+  phoneNumber?: string;
+  email?: string;
+  emailVerified?: boolean;
+  displayName?: string;
+  photoURL?: string;
+  postCode?: string;
+  address?: string;
+  addressDetail?: string;
+};
+
 export default class User {
   uid: string;
   displayName?: string;
