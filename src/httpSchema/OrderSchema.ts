97c252bf774f033e2e58255a138e4b8aa2b6fa77
uxdx <uxdx@naver.com>
2023-09-14 21:46:40 +0900

    1.1.2: OrderSchema: add updatedAt, state info

--- src/httpSchema/OrderSchema.ts
+++ src/httpSchema/OrderSchema.ts
@@ -1,5 +1,6 @@
 export type OrderSchema = {
   uid: string;
+  orderId?: string;
   data: {
     id: string;
     quantity: number;
@@ -7,4 +8,6 @@ export type OrderSchema = {
     additionalRequests?: string;
   }[];
   createdAt?: number; // Timestamp.now().toMillis()
+  updatedAt?: number; // Timestamp.now().toMillis()
+  state?: 'PENDING' | 'APPROVE' | 'REJECT' | 'SHIPPING' | 'SHIPPED';
 };
