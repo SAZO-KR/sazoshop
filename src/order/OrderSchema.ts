c7e071b14e78231647e0b564e406f5e991375abe
uxdx <uxdx@naver.com>
2023-09-26 22:20:34 +0900

    1.1.9: OrderSchema modified

--- src/order/OrderSchema.ts
+++ src/order/OrderSchema.ts
@@ -2,13 +2,13 @@ import {UserData} from '../user/User';
 
 export type OrderSchema = {
   uid?: string; // User.uid
-  orderId?: string; // 주문번호 서버측에서 생성
+  orderId?: string; // 주문번호
   data: {
     id: string; // 상품 id (prdInfo.hash())
     quantity: number; // 수량 > 0
     prdInfo: string; // 상품 정보  (JSON.stringify(prdInfo.toJSON()))
-    additionalRequests?: string; // 추가 요청사항
   }[];
+  additionalRequests?: string; // 추가 요청사항
   createdAt?: number; // Timestamp.now().toMillis() 서버측에서 생성
   updatedAt?: number; // Timestamp.now().toMillis() 서버측에서 생성
   state?: 'PENDING' | 'APPROVE' | 'REJECT' | 'SHIPPING' | 'SHIPPED'; // 서버측에서 관리
