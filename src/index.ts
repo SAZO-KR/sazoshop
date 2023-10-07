db7b3795636107efdcb657f29037cc1c9ce09a3f
Dongyeop Kim <uxdx@naver.com>
2023-10-07 15:52:24 +0900

    2.0.0: Product Info Arranged.

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,25 @@ import {
   PrdAttr,
   PrdAttrBuilder,
 } from './prdInfo';
+import {
+  ProductInfo,
+  ProductInfoBuilder,
+  ProductOption,
+  ProductOptionBuilder,
+  ProductAttribute,
+  ProductAttributeBuilder,
+} from './productInfo';
 import {OptionTypeError, OptionNoAttrError} from './prdInfo/Error';
 import {UserData} from './user/User';
 import {OrderSchema} from './order/OrderSchema';
 
 export {
+  ProductInfo,
+  ProductInfoBuilder,
+  ProductOption,
+  ProductOptionBuilder,
+  ProductAttribute,
+  ProductAttributeBuilder,
   PrdInfo,
   PrdInfoBuilder,
   PrdOptionBuilder,
---
+++ src/productInfo/index.ts
@@ -0,0 +1,15 @@
+import ProductInfo from './ProductInfo';
+import ProductOption from './ProductOption';
+import ProductAttribute from './ProductAttribute';
+import ProductInfoBuilder from './ProductInfoBuilder';
+import ProductOptionBuilder from './ProductOptionBuilder';
+import ProductAttributeBuilder from './ProductAttributeBuilder';
+
+export {
+  ProductInfo,
+  ProductOption,
+  ProductAttribute,
+  ProductInfoBuilder,
+  ProductOptionBuilder,
+  ProductAttributeBuilder,
+};
