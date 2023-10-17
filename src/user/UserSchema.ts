06df6b3abd9b1a773510f0b5f56529e83840beac
Dongyeop Kim <uxdx@naver.com>
2023-10-17 21:03:17 +0900

    2.2.8: UserSchema.ts modified. .prettierrc modified.

--- src/user/UserSchema.ts
+++ src/user/UserSchema.ts
@@ -1,5 +1,5 @@
 export type UserSchema = {
-  [key: string]: string | boolean | undefined;
+  [key: string]: string | number | boolean | undefined | object[];
   uid?: string;
   firstName?: string;
   lastName?: string;
@@ -11,7 +11,28 @@ export type UserSchema = {
   emailVerified?: boolean;
   displayName?: string;
   photoURL?: string;
-  postCode?: string;
-  address?: string;
-  addressDetail?: string;
+  // 주소
+  addresses?: {
+    isDefault?: boolean;
+    postalCode?: string;
+    address?: string;
+    addressDetail?: string;
+  }[];
+  // 쿠폰
+  coupons?: {
+    id?: string;
+    name?: string;
+    description?: string;
+    discount?: number;
+    type?: 'PERCENT' | 'PRICE';
+    createdAt: number;
+    expiredAt: number;
+  }[];
+  // 포인트
+  points?: {
+    amount: number;
+    description?: string;
+    createdAt?: number;
+    expiredAt: number;
+  }[];
 };
