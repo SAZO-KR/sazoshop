3b56c60f8d54511735b2cf5dc9a91a3b1fef8fe9
Dongyeop Kim <uxdx@naver.com>
2023-11-04 19:37:58 +0900

    :label: 2.4.0: TariffItemSchema.ts updated.

--- src/tariff/TariffItemSchema.ts
+++ src/tariff/TariffItemSchema.ts
@@ -1,13 +1,13 @@
 export type TariffItemSchema = {
-  type: string;
+  type: 'CATEGORY' | 'ITEM'; // CATEGORY | ITEM
   description: string;
   rate: {
-    general: string;
-    temporary: string;
-    wto: string;
-    gsp: string;
-    ldc: string;
-    epa: string;
+    general: string; // 基本税率
+    temporary: string; // 暫定税率
+    wto: string; // WTO協定;
+    gsp: string; // 特恵(一般特恵対象国)
+    ldc: string; // 特別特恵(特別特恵税率)
+    epa: string; // EPA税率(経済連携協定税率)
   };
   code?: string;
   embeddings?: number[];
