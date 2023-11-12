9d743e4de9d8fb987007336249e6e4e814806922
uxdx <uxdx@naver.com>
2023-11-12 23:28:52 +0900

    :label: 2.4.6: SearchResponseSchema updated.

--- src/search/SearchSchema.ts
+++ src/search/SearchSchema.ts
@@ -1,39 +1,43 @@
 export type SearchResponseSchema = {
-  productName: string;
-  translatedProductName: string;
-  maker: string;
-  translatedMaker: string;
-  brand: string;
-  translatedBrand: string;
-  imageUrl: string;
-  originalLowPrice: number;
-  exchangeLowPrice: number;
-  reviewCount: number;
-  starRating: number;
-  keepCnt: number;
-  attribute: {
-    attributeValue: string;
-    translatedAttributeValue: string;
-    characterValue: string;
-    translatedCharacterValue: string;
+  originQuery: string;
+  translatedQuery: string;
+  data: {
+    productName: string;
+    translatedProductName: string;
+    maker: string;
+    translatedMaker: string;
+    brand: string;
+    translatedBrand: string;
+    imageUrl: string;
+    originalLowPrice: number;
+    exchangeLowPrice: number;
+    reviewCount: number;
+    starRating: number;
+    keepCnt: number;
+    attribute: {
+      attributeValue: string;
+      translatedAttributeValue: string;
+      characterValue: string;
+      translatedCharacterValue: string;
+    };
+    openDate: string;
+    category: {
+      category1Name: string;
+      translatedCategory1Name: string;
+      category2Name: string;
+      translatedCategory2Name: string;
+      category3Name: string;
+      translatedCategory3Name: string;
+      category4Name: string;
+      translatedCategory4Name: string;
+    };
+    mallList: {
+      mallName: string;
+      translatedMallName: string;
+      mallPid: string;
+      price: number;
+      exchangePrice: number;
+      pcMallUrl: string;
+    }[];
   };
-  openDate: string;
-  category: {
-    category1Name: string;
-    translatedCategory1Name: string;
-    category2Name: string;
-    translatedCategory2Name: string;
-    category3Name: string;
-    translatedCategory3Name: string;
-    category4Name: string;
-    translatedCategory4Name: string;
-  };
-  mallList: {
-    mallName: string;
-    translatedMallName: string;
-    mallPid: string;
-    price: number;
-    exchangePrice: number;
-    pcMallUrl: string;
-  }[];
 };
