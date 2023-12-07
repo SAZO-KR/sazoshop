48f07554b016a2676c5a482ea266afbb814b9db9
Dongyeop Kim <uxdx@naver.com>
2023-12-07 21:49:03 +0900

    :label: 2.6.0: ProductCardSchema updated

--- src/productInfo/ProductCardSchema.ts
+++ src/productInfo/ProductCardSchema.ts
@@ -1,11 +1,8 @@
 type ProductCardSchema = {
   productName: string; // 상품명
   translatedProductName: string; // 번역된 상품명
-  originalPrice: number; // 원래 가격
-  discountPrice: number; // 할인된 가격
-  exchangeOriginalPrice: number; // 환율 적용 가격
-  exchangeDiscountPrice: number; // 환율 적용 가격
-  discountRate: number; // 할인율
+  price: number; // 원래 가격
+  exchangedPrice: number; // 환율 적용 가격
   mallurl: string; // 쇼핑몰 URL
   imageUrl: string; // 상품 이미지 URL
   expectedshippingfee?: number; // 예상 배송비
