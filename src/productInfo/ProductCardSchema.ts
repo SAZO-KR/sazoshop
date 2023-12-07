9ddb061b502b164f5720d287fb3471674fe79fd9
Dongyeop Kim <uxdx@naver.com>
2023-12-07 22:01:06 +0900

    :label: 2.6.3: mallurl to mallUrl

--- src/productInfo/ProductCardSchema.ts
+++ src/productInfo/ProductCardSchema.ts
@@ -4,7 +4,7 @@ type ProductCardSchema = {
   translatedProductName: string; // 번역된 상품명
   price: number; // 원래 가격
   exchangedPrice: number; // 환율 적용 가격
-  mallurl: string; // 쇼핑몰 URL
+  mallUrl: string; // 쇼핑몰 URL
   imageUrl: string; // 상품 이미지 URL
   expectedshippingfee?: number; // 예상 배송비
   starRating?: number; // 별점
